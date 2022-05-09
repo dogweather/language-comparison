c7fe9e8768a66ccc381e9146839fe525ff68f979
Robb Shecter <robb@public.law>
2022-05-08 19:30:45 -0600

    crystal: docs

--- crystal/src/text_service.cr
+++ crystal/src/text_service.cr
@@ -1,11 +1,20 @@
+# Plaintext and HTML manipulation.
 module TextService
-  VERSION = "0.1.0"
-
+  #
+  # Return a new string enhanced with typographic characters:
+  #  Single quotes: ’
+  #  Double quotes: “”
+  #
   def self.add_typography(text : String) : String
     text.gsub(/"([^"]+)"/, "“\\1”")
         .gsub("'", "’")
   end
 
+
+  #
+  # Add nicer typography that HTML can provide:
+  #  Fractions using superscript and subscript.
+  #
   def self.add_html_typography(text : String) : String
     text.gsub(%r{\b(\d+)/(\d+)\b}, "<sup>\\1</sup>&frasl;<sub>\\2</sub>")
   end
