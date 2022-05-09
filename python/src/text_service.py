5c1ffe2612e53e3d34ed457a5c33dff85b1da6ba
Robb Shecter <robb@public.law>
2022-05-08 20:59:30 -0600

    python: two passing tests

--- python/src/text_service.py
+++ python/src/text_service.py
@@ -4,6 +4,10 @@ DOUBLE_QUOTED = re.compile(r'"([^"]+)"')
 
 
 def add_typography(text: str) -> str:
+    """Return a new string enhanced with typographic characters:
+         Single quotes: ’
+          Double quotes: “”
+    """
     return DOUBLE_QUOTED.sub(r'“\1”', text).replace("'", "’")
 
 
