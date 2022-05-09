584bf5e470635faa0893ec865580037360646531
Robb Shecter <robb@public.law>
2022-05-08 22:55:30 -0600

    elixir: all tests pass

--- elixir/lib/text_service.ex
+++ elixir/lib/text_service.ex
@@ -1,5 +1,6 @@
 import String
 
+
 defmodule TextService do
   @moduledoc """
   Documentation for `TextService`.
@@ -10,5 +11,10 @@ defmodule TextService do
     text
     |> replace(~r/\"([^\"]+)\"/, "“\\1”")
     |> replace(~r/'/, "’")
+  end
+
+  def add_html_typography(text) do
+    text
+    |> replace(~r/\b(\d+)\/(\d+)\b/, "<sup>\\1</sup>&frasl;<sub>\\2</sub>")
   end
 end
--- elixir/test/text_service_test.exs
+++ elixir/test/text_service_test.exs
@@ -1,12 +1,31 @@
+import TextService, only: [add_typography: 1, add_html_typography: 1]
+
+
 defmodule TextServiceTest do
   use ExUnit.Case
   doctest TextService
 
+  #
+  # .add_typography
+  #
+
   test "converts single quotes" do
-    assert TextService.add_typography("dog's life") == "dog’s life"
+    assert add_typography("dog's life") == "dog’s life"
   end
 
   test "converts double quotes" do
-    assert TextService.add_typography("\"Hey!\"") == "“Hey!”"
+    assert add_typography("\"Hey!\"") == "“Hey!”"
+  end
+
+
+  #
+  # .add_html_typography
+  #
+
+  test "creates simple fractions" do
+    plaintext = "on 1/2 of an egg"
+    expected  = "on <sup>1</sup>&frasl;<sub>2</sub> of an egg"
+
+    assert add_html_typography(plaintext) == expected
   end
 end
