8ff890821458f85c8f0e8b06dd5a1345e9e1b199
Robb Shecter <robb@public.law>
2022-05-08 20:58:11 -0600

    python: two passing tests

--- python/src/text_service.py
+++ python/src/text_service.py
@@ -1,5 +1,10 @@
+import re
+
+DOUBLE_QUOTED = re.compile(r'"([^"]+)"')
+
+
 def add_typography(text: str) -> str:
-    return text.replace("'", "’")
+    return DOUBLE_QUOTED.sub(r'“\1”', text).replace("'", "’")
 
 
 def add_html_typography(text: str) -> str:
