9c9caee866bb2bc4d59bff5e014259de66b1f071
Robb Shecter <robb@public.law>
2022-05-08 19:16:16 -0600

    crystal: proper failure

--- crystal_app/spec/text_service_spec.cr
+++ crystal_app/spec/text_service_spec.cr
@@ -1,11 +1,21 @@
 require "./spec_helper"
 
 describe TextService do
-  it "works for a single quote" do
-    TextService.double_quotes("Robb's dog").should eq("Robb’s dog")
+  describe "add_typography()" do
+    it "converts single quotes" do
+      expect("dog's life".add_typography).to eq "dog’s life"
+    end
+
+    it "converts double quotes" do
+      expect("\"Hey!\"".add_typography).to eq "“Hey!”"
+    end
   end
 
-  it "works for double quotes" do
-    TextService.double_quotes("\"Hello\"").should eq("“Hello”")
+  describe "add_html_typography()" do
+    it "creates simple fractions" do
+      plaintext = "on 1/2 of an egg"
+      expected = "on <sup>1</sup>&frasl;<sub>2</sub> of an egg"
+      expect(plaintext.add_html_typography).to eq expected
+    end
   end
 end
