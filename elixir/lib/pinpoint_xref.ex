b9ea900e731e00337a9282e9fa4f020013f8f778
Robb Shecter <robb@public.law>
2022-06-20 20:28:30 -0600

    elixir: refactor

--- elixir/lib/pinpoint_xref.ex
+++ elixir/lib/pinpoint_xref.ex
@@ -1,11 +1,13 @@
+import String
+
 defmodule PinpointXref do
   @moduledoc """
   Code for adding pinpoint citation cross-reference links.
   """
 
   def add_pinpoint(text) do
-    String.replace(
-      text,
+    text
+    |> replace(
       ~r/">article (\d+)<\/a>, paragraph (\d+)/,
       ~s[#\\g{2}">article \\g{1}, paragraph \\g{2}</a>]
     )
