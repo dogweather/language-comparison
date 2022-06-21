a96ab16b181f63f76df3859bdab9c08cca787d7e
Robb Shecter <robb@public.law>
2022-06-20 19:11:53 -0600

    rust fixes

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,24 +1,29 @@
 #![allow(dead_code)]
 
 /// Plaintext and HTML manipulation.
-
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::borrow::Cow;
 
 lazy_static! {
     static ref DOUBLE_QUOTED_TEXT: Regex = Regex::new(r#""(?P<content>[^"]+)""#).unwrap();
-    static ref SINGLE_QUOTE:       Regex = Regex::new(r"'").unwrap();
-    static ref FRACTION:           Regex = Regex::new(r"\b(\d+)/(\d+)\b").unwrap();
+    static ref SINGLE_QUOTE: Regex = Regex::new(r"'").unwrap();
+    static ref FRACTION: Regex = Regex::new(r"\b(\d+)/(\d+)\b").unwrap();
 }
 
+/// Return a new string enhanced with typographic characters:
+///     Single quotes: ’
+///     Double quotes: “”
+fn add_typography(text: &str) -> String {
+    let with_single_quotes = add_single_quotes(text);
+    add_double_quotes(&with_single_quotes).into_owned()
+}
 
 /// Return a new string enhanced with typographic characters:
 ///     Double quotes: “”
 fn add_double_quotes(text: &str) -> Cow<str> {
     DOUBLE_QUOTED_TEXT.replace_all(text, "“$content”")
 }
-
 
 /// Return a new string enhanced with typographic characters:
 ///     Single quotes: ’
@@ -27,22 +32,12 @@ fn add_single_quotes(text: &str) -> Cow<str> {
     SINGLE_QUOTE.replace_all(text, "’")
 }
 
-
-/// Return a new string enhanced with typographic characters:
-///     Single quotes: ’
-///     Double quotes: “”
-fn add_typography(text: &str) -> Cow<str> {
-    add_double_quotes(&add_single_quotes(text))
-}
-
-
 /// Add nicer typography that HTML can provide:
 ///     Fractions using superscript and subscript.
 ///
 fn add_html_typography(text: &str) -> Cow<str> {
     FRACTION.replace_all(text, r"<sup>$1</sup>&frasl;<sub>$2</sub>")
 }
-
 
 #[cfg(test)]
 mod tests {
