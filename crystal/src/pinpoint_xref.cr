7b0fc0ed4fa0ae264710e21fa0b90d2fde5a66a5
Robb Shecter <robb@public.law>
2022-06-20 21:12:43 -0600

    crystal: refactor

--- crystal/src/pinpoint_xref.cr
+++ crystal/src/pinpoint_xref.cr
@@ -2,9 +2,10 @@ module PinpointXref
   extend self
 
   def add_pinpoint(html)
-    html.gsub(
-      /">article (\d+)<\/a>, paragraph (\d+)/,
-      "#\\2\">article \\1, paragraph \\2</a>"
-    )
+    html
+      .gsub(
+        /">article (\d+)<\/a>, paragraph (\d+)/,
+        "#\\2\">article \\1, paragraph \\2</a>"
+      )
   end
 end
