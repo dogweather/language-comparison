b1cb7c19cf27562761299295606e8d34456d0e56
Robb Shecter <robb@public.law>
2022-06-23 00:08:59 -0600

    checkpoint

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -89,9 +89,14 @@ mod tests {
 
     #[test]
     fn adds_simple_xref() {
-        let without = r##"<section class="level-0 non-meta outline" id="2"><h2><a title="Permanent link" href="#2">2.</a></h2>If a State becomes a Party to this Statute after its entry into force, the Court may exercise its jurisdiction only with respect to crimes committed after the entry into force of this Statute for that State, unless that State has made a declaration under <a data-toggle="tooltip" title="Preconditions to the exercise of jurisdiction" href="article_12_preconditions_to_the_exercise_of_jurisdiction">article 12</a>, paragraph 3.</section>"##;
-        let with___ = r##"<section class="level-0 non-meta outline" id="2"><h2><a title="Permanent link" href="#2">2.</a></h2>If a State becomes a Party to this Statute after its entry into force, the Court may exercise its jurisdiction only with respect to crimes committed after the entry into force of this Statute for that State, unless that State has made a declaration under <a data-toggle="tooltip" title="Preconditions to the exercise of jurisdiction" href="article_12_preconditions_to_the_exercise_of_jurisdiction#3">article 12, paragraph 3</a>.</section>"##;
+        const without: &str = r##"<section class="level-0 non-meta outline" id="2"><h2><a title="Permanent link" href="#2">2.</a></h2>If a State becomes a Party to this Statute after its entry into force, the Court may exercise its jurisdiction only with respect to crimes committed after the entry into force of this Statute for that State, unless that State has made a declaration under <a data-toggle="tooltip" title="Preconditions to the exercise of jurisdiction" href="article_12_preconditions_to_the_exercise_of_jurisdiction">article 12</a>, paragraph 3.</section>"##;
+        const with___: &str = r##"<section class="level-0 non-meta outline" id="2"><h2><a title="Permanent link" href="#2">2.</a></h2>If a State becomes a Party to this Statute after its entry into force, the Court may exercise its jurisdiction only with respect to crimes committed after the entry into force of this Statute for that State, unless that State has made a declaration under <a data-toggle="tooltip" title="Preconditions to the exercise of jurisdiction" href="article_12_preconditions_to_the_exercise_of_jurisdiction#3">article 12, paragraph 3</a>.</section>"##;
 
+        assert_eq!(add_pinpoint(""), "");
         assert_eq!(add_pinpoint(without), with___);
+        assert_eq!(
+            add_pinpoint(concat!(without, without)),
+            with___.to_owned() + with___
+        );
     }
 }
